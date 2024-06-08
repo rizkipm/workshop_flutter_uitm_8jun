d68242d8861b9cc04c668360fdb1214b0529c6a1
Rizki Pm <udarizki2020@gmail.com>
2024-06-08 13:58:09 +0700

    update maps

--- lib/screen/page_detail_univ.dart
+++ lib/screen/page_detail_univ.dart
@@ -15,8 +15,36 @@ class PageDetailUniv extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('${university.univName}'),
+        title: Text('${university.univName}',  style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.purple,
       ),
+
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10,),
+        child : Column(
+          crossAxisAlignment : CrossAxisAlignment.start,
+          children:[
+            ClipRRect(
+              borderRadius : BorderRadius.circular(10),
+              child : Image.network('http://192.168.1.3:8080/server_university/images/${data?.univImage}',
+                height: 100,
+                width : double.infinity
+                fit: BoxFit.fill,
+              ),
+            ),
+            Sizebox(height : 10),
+            Text(university.univName),
+            Sizebox(height : 10),
+            Text(university.univ_address),
+            Sizebox(height : 10),
+            Text(university.univ_email),
+            Sizebox(height : 10),
+            Text(university.univ_desc),
+
+          ]
+        )
+
+      )
     );
   }
 }
