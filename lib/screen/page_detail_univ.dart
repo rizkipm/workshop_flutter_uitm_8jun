c5c23414055bb5cf9eb8d3ea262f295ff3df53e1
Rizki Pm <udarizki2020@gmail.com>
2024-06-08 11:53:41 +0700

    update

---
+++ lib/screen/page_detail_univ.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class PageDetailUniv extends StatefulWidget {
+  const PageDetailUniv({super.key});
+
+  @override
+  State<PageDetailUniv> createState() => _PageDetailUnivState();
+}
+
+class _PageDetailUnivState extends State<PageDetailUniv> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
